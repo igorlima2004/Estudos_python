82494e4cb111d1bdbae3018aa3a12cd235daafc6
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-09-26 18:12:59 -0300

    Exercicios 1
    lançados os exercícios sobre comentários e tipos de variáveis

---
+++ FUNDAMENTOS/comentarios.py
@@ -0,0 +1,18 @@
+#Comentário de uma linha
+
+"""
+comentário 
+de
+multiplas
+linhas
+"""
+salario = 1000
+desconto = 200
+"""
+O desconto é multiplicado por 2 em 
+razão das faltas contabilizadas, tendo mais 
+de 3 faltas  o desconto é dobrado 
+"""
+total = salario - (2 * desconto)
+print(total)
+print("Código fonte")
---
+++ FUNDAMENTOS/tipos_dados_variaveis.py
@@ -0,0 +1,16 @@
+#Booleanos(bool)
+print(True)
+print(False)
+#Float
+print(1.5 + 3.5)
+#String
+print("Igor")
+print('também texto')
+
+#Variável
+altura = 1.73
+peso = 72
+
+imc = peso / (altura * altura)
+#Constante: não tem uma definição correta...é recomendado escrever uma constante com LETRAS MAIUSCULAS
+PI = 3.14
